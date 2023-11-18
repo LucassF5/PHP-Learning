961a3f38dafb4c703978c7c25721f1e6631f8e30
L F <franco100lucas@gmail.com>
2023-11-18 19:05:36 -0300

    Add comments explaining the use of sizeof and count functions

--- Arrays.php
+++ Arrays.php
@@ -7,6 +7,8 @@ $newArray = [1,2,3,4,5,6];
 // echo "$newArray[0]".PHP_EOL;
 
 // count($arrayList) Vai contar todos os elementos no array
+// sizeof($arrayList) também vai fazer a mesma coisa que count
+// sizeof e count são duas funções que fazem a mesma coisa
 
 for ($i = 0; $i < count($newArray); $i++) {
     // echo $newArray[$i].PHP_EOL; 
