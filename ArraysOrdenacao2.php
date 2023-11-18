60a3473099102b4bf35d796e9605b53abf6cbe8c
L F <franco100lucas@gmail.com>
2023-11-18 19:29:28 -0300

    Ordenando arrays parte 2

---
+++ ArraysOrdenacao2.php
@@ -0,0 +1,40 @@
+<?php
+
+$notas =[
+    [
+        "aluno" => "Lucas",
+        'nota'=> 10,
+    ],
+    [
+        "aluno" => "Maria",
+        'nota'=> 8,
+    ],
+    [
+        "aluno" => "Fm",
+        'nota'=> 9,
+    ]
+];
+
+function ordenaNotas(array $nota1, array $nota2): int
+{   
+    if ($nota1["nota"]>$nota2["nota"]){
+        return -1;
+    } else if ($nota1["nota"]<$nota2["nota"]){
+        return 1;
+    }
+
+    return 0;
+}
+
+usort($notas,"ordenaNotas");
+// Método de ordenação que espera um array(que vai ser ordenado) 
+// e uma função, para especificar como ele deve ordenar, que retorna um inteiro
+//1(se for maior/precisar vir antes), === 0(se forem iguais), -1(se for menor/precisar vir depois)
+
+// Como o método usort espera uma função como parâmetro, 
+// logo pode-se só colocar o nome da função entre aspas
+
+var_dump($notas);
+//Ordenou 
+
+?>
