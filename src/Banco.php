6b54b3f6e2a4d2125ffc955ad32c3fa46baf631a
L F <franco100lucas@gmail.com>
2023-11-24 14:07:42 -0300

    Refatorando atributos da Conta para privados

---
+++ src/Banco.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "src/Conta.php";
+
+$primeiraConta = new Conta();
+// $primeiraConta->nomeTitular = "Lucas";
+$primeiraConta->setNome("Lucas");
+$primeiraConta->setCPF("123.456.789-10");
+$primeiraConta->depositar(500);
+$primeiraConta->sacar(300);
+// var_dump($primeiraConta);
+echo $primeiraConta->getNome().PHP_EOL;
+echo $primeiraConta->getCPF().PHP_EOL;
+echo $primeiraConta->getSaldo().PHP_EOL;
+
+?>
--- src/Conta.php
+++ src/Conta.php
@@ -1,10 +1,10 @@
 <?php
 
 class Conta{
-    public string $cpfTitular;
+    private string $cpfTitular;
     //Pode-se definir o tipo de dado que o atributo irá receber
-    public $nomeTitular;
-    public $saldo = 0;
+    private $nomeTitular;
+    private $saldo = 0;
 
     public function sacar(float $valorASacar){
         if($valorASacar>$this->saldo){
@@ -38,16 +38,42 @@ class Conta{
         $this->depositar($valorATransferir);
     }
     //Utilizando Early Return, evita-se o uso de else
+
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function getCPF(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nomeTitular;
+    }
+    
+    public function setNome(string $nome): void
+    {
+        $this->nomeTitular = $nome;
+    }
+    
+    public function setCPF(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
+
 }
 
-$primeiraConta = new Conta();
+// $primeiraConta = new Conta();
 
-$primeiraConta->saldo = 100;
-//Para acessar o valor de um atributo, usa-se o operador ->
-$primeiraConta->cpfTitular = "123.456.789-00";
-$primeiraConta->nomeTitular = "Lucas Franco";
+// $primeiraConta->saldo = 100;
+// //Para acessar o valor de um atributo, usa-se o operador ->
+// $primeiraConta->cpfTitular = "123.456.789-00";
+// $primeiraConta->nomeTitular = "Lucas Franco";
 
-var_dump($primeiraConta);
+// var_dump($primeiraConta);
 
 // echo $primeiraConta->nomeTitular.PHP_EOL;
 // echo $primeiraConta->cpfTitular.PHP_EOL;
